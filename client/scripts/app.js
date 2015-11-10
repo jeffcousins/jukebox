b0fe07da381a4914f53abe50c870c3945d4e71de
Jeff Cousins <jeffcousins@me.com>
2015-11-09 18:26:21 -0600

    input field capture and basic soundcloud get request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,60 @@
-// Your ReactJS code here
 
+
+// Components:
+  //  App
+  //  SearchBar
+  //  SongResultsDisplay
+  //  Song --> title, artist, etc
+  //  Playlist
+
+// songProps
+// artwork_url
+// genre
+// duration
+// title
+// user --> user object, contains user profile data
+// state? --> "finished"
+// premalink_url --> auto play opens on soundcloud
+// id --> num --> streaming = /tracks/id
+
+var App = React.createClass({
+  render: function() {
+    return (<div className="container">
+        Hola, world
+        <SearchBar />
+      </div>);
+  }
+});
+
+var SearchBar = React.createClass({
+  getSongs: function(query) {
+    SC.initialize({
+      client_id: '9cf8c397a36be5e79964b5c800788392'
+    });
+    SC.get('/tracks', {
+      q: query,
+      license: 'cc-by-sa'
+    }).then(function(tracks) {
+      console.log('tracks/' + tracks[0].id);
+      SC.stream('/tracks/' + tracks[0].id).then(function(player){
+        player.play();
+      });
+    });
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var searchQuery = this.refs.search.getDOMNode().value;
+    console.log(searchQuery);
+    this.getSongs(searchQuery);
+    this.refs.search.getDOMNode().value = '';
+  },
+  render: function() {
+    return(<form onSubmit={this.handleSubmit}>
+              Search For a Song or Aritst: <br />
+              <input type="text" id="search" defaultValue="" ref="search"></input>
+              <button type="submit" placeholder="Jimi Hendrix">Search</button>
+           </form>)
+  }
+});
+
+React.render(<App />, document.body)
--- client/scripts/config.js
+++ client/scripts/config.js
@@ -1,1 +1,16 @@
-var SOUND_CLOUD_KEY =  'ENTER YOUR SOUND CLOUD KEY';
+var SOUND_CLOUD_KEY =  '9cf8c397a36be5e79964b5c800788392';
+
+
+var getSongs = function(query) {
+  SC.initialize({
+    client_id: SOUND_CLOUD_KEY
+  });
+
+  SC.get('/tracks', {
+    q: query,
+    license: 'cc-by-sa'
+  }).then(function(tracks) {
+    console.log(tracks['genre']);
+    console.log(tracks[0].description);
+  });
+}

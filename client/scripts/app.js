d8e3118a4eacbbc75b0021ad099ee9ff190603dc
Jeff Cousins <jeffcousins@me.com>
2016-04-14 13:47:53 -0500

    old commits I forgot to update

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,26 +1,36 @@
 /** @jsx React.DOM */
 
+  $(window).scroll(function(){
+        $('.playerQueue').css('left', 0 - $(this).scrollLeft());
+    });
+
+
 var App = React.createClass({
   getInitialState: function() {
     return {searchString: '',
-            songs: []};
+            songs: [],
+            playerQueue: []};
   },
   loadSongs: getSongs,
-  handleChange: function(childString) {
+  handleSearch: function(childString) {
     this.setState({searchString: childString});
   },
-  logger: function() {
-    console.log(this.state);
+  handleQueue: function(song) {
+    var newPlayerQueue = this.state.playerQueue;
+    console.log(newPlayerQueue.push(song));
+    this.setState({playerQueue: newPlayerQueue});
+    console.log(this.state.playerQueue)
   },
   render: function() {
     return (<div className="container">
             <div className="row">
             Main App
-              <SearchBar loadSongs={this.loadSongs} searchString={this.state.searchString} grabSearch={this.handleChange} />
-              <SongQueue boxHeader="Song Queue" songs={this.state.songs} />
-              <button onClick={this.logger}>Logger</button>
+              <SearchBar loadSongs={this.loadSongs} grabSearch={this.handleSearch} />
+              <SongQueue songs={this.state.songs} handleQueue={this.handleQueue} />
             </div>
-            </div>)
+            <Player songs={this.state.playerQueue} handleQueue={this.handleQueue} />
+            </div>
+            )
   }
 });
 
@@ -32,14 +42,18 @@ var SearchBar = React.createClass({
   },
   render: function() {
     return(<div>
-              Search For a Song or Aritst: <br />
+              Search For a Song or Artist: <br />
               <input type="text" placeholder="type here" ref="userInput"></input>
               <button type="button" onClick={this.sendToParent} placeholder="">Search</button>
            </div>)
   }
 });
 
+// this.props.handleQueue
 var Song = React.createClass({
+  addToPlayer: function() {
+    this.props.handleQueue(this.props.song);
+  },
   render: function() {
     return (
 
@@ -48,19 +62,27 @@ var Song = React.createClass({
               <div className="card-block">
                 <h5 className="card-title">{this.props.song.title.slice(0, 15)}</h5>
                 <p className="card-text">{this.props.song.genre || 'Disco'} - {this.props.song['release_year'] || 2015}</p>
-                <a href="#" className="btn btn-primary">Play | Add</a>
+                <a href="#" className="btn btn-primary" onClick={this.addToPlayer}>Play | Add</a>
             </div>
           </div>
           )
   }
 });
 
+// for search results
+// new SongQueue songs={this.state.searchresults}
+
+// for play next queue
+// new SongQueue songs={this.state.songQueue}
+
 // passed in when rendering SongQueue: <SongQueue boxHeader={SearchBar.getSongs}/>
+// passed in when rendering SongQueue: handleQueue={this.handleQueue} --- this.props.handleQueue
 var SongQueue = React.createClass({
+  
   render: function() {
-    // pass in all found songs --> this.props.songs
-    var songs = this.props.songs.map(function(song) {
-      return <Song song={song} key={song.title}/>
+    var context = this;// pass in all found songs --> this.props.songs
+    var songs = context.props.songs.map(function(song) {
+      return <Song song={song} key={song.title} handleQueue={context.props.handleQueue} />
     });
 
     return (<div className="container">
@@ -70,41 +92,50 @@ var SongQueue = React.createClass({
             </div>)
   }
 });
-
+      // SC.stream('/tracks/' + tracks[0].id).then(function(player){
+      //   player.play();
+      // });
 
 var Controls = React.createClass({
   getInitialState: function() {
     return {currentSong: '',
             currentPicture: 'https:i1.sndcdn.com/artworks-000077325920-ecs8lu-large.jpg'};
   },
+  playNow: function() {
+    var context = this;
+    SC.stream('/tracks/' + context.props.song.id).then(function(player){
+      player.play();
+    });
+  },
   render: function() {
-    return (<div className="container" id='controller'>
-            <img id="currentPicture"src={this.state.currentPicture} /><br />
-        <button type="button" className="btn btn-default" aria-label="Left Align" id="play"><span className="glyphicon glyphicon-play"></span></button>
-        <button type="button" className="btn btn-default" aria-label="Left Align" id="pause"><span className="glyphicon glyphicon-pause"></span></button>
-        <button type="button" className="btn btn-default" aria-label="Left Align" id="stop"><span className="glyphicon glyphicon-stop"></span></button>
+    return (<div id='controller' className="col-xs-3">
+          <img id="currentPicture"src={this.state.currentPicture} style={{"opacity" :"1", "z-index": "5"}} /><br />
+          <button onClick={this.playNow}type="button" className="btn btn-default" aria-label="Left Align" id="play"><span className="glyphicon glyphicon-play"></span></button>
+          <button type="button" className="btn btn-default" aria-label="Left Align" id="pause"><span className="glyphicon glyphicon-pause"></span></button>
+          <button type="button" className="btn btn-default" aria-label="Left Align" id="stop"><span className="glyphicon glyphicon-stop"></span></button>
       </div>)
   }
 });
-
+//this.props.songs === App playerQueue array
+//                    [  [ ]   [ ]   [ ]   [ ]   [ ]  ] className={this.props.colsize}
+                    // [           ] [ [] [] [] [] []  ] 
 var Player = React.createClass({
   render: function() {
-    return (    
-            <div className="container">
-              <Controls />
-
-              <p className="text-muted">Place sticky footer content here.</p>
-            </div>)
+    return (
+            <footer className="footer-fixed-bottom playerQueue" id="footer">
+              <div className="container">
+                <div className='row'>
+                  <Controls song={this.props.songs[0]} />
+                  <div className="col-xs-9 " >
+                    <SongQueue songs={this.props.songs} />
+                  </div>
+                </div>
+              </div>
+            </footer>)
   }
 });
 
 React.render(<App />, document.getElementById('main'))
-React.render(<Player />, document.getElementById('footer'))
-
-
-
-
-
 
 
 // <div className="container">
@@ -186,9 +217,9 @@ React.render(<Player />, document.getElementById('footer'))
 //       console.log(tracks);
 //       list = tracks;
 //       console.log('tracks/' + tracks[0].id);
-//       // SC.stream('/tracks/' + tracks[0].id).then(function(player){
-//       //   player.play();
-//       // });
+      // SC.stream('/tracks/' + tracks[0].id).then(function(player){
+      //   player.play();
+      // });
 //     });
 //   },
 //   handleSubmit: function(e) {
